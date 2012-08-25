bbdd992ed092d66003a09eea202054d25366f769
furiston <furiston@gmail.com>
2012-08-25 18:02:21 +0300

    handled saving data to .csv file

--- gd/getdemocracy.py
+++ gd/getdemocracy.py
@@ -1,21 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import urllib2
 from bs4 import BeautifulSoup
 import re, os, sys, csv
 import gdsettings as gds
 
-mv_info = {}
 mv_ids = []
 mv_all_work = {}
-def mv_specs():
+parties = []
+def main():
    html = BeautifulSoup(urllib2.urlopen(gds.MV_LIST_LINK), from_encoding="iso-8859-9")   
    city = ""
    mvno = ""
    mvname = ""
    mvparty = ""
    parseData = html.table.table.table.find_all('td', align="left")
-   dataToArray = str(parseData).split(', ')[1:-1]
+   dataToArray = []
+   d = []
 
-   # print dataToArray
+   for info in parseData:
+      d.append(info)
+
+   for x in d:
+      dataToArray.append(str(x))
+
+   listOfMv = csv.writer(open('../mvlist.csv', 'w'))
+   listOfParties = csv.writer(open('../partylist.csv', 'w'))
 
    for data in dataToArray:
       if '<b>' in data:
@@ -28,20 +39,24 @@ def mv_specs():
 
       if not '<b>' in data and not 'href' in data:
          mvparty = data[17:-5]
-         mv_info[mvno] = mvname, mvparty, city
-
-   print mv_info
-
-   datatocsv = csv.DictWriter(open('../mvdb.csv', 'w'), mv_info.keys())
-   datatocsv.writeheader()
-   datatocsv.writerow(mv_info)
-
-   print "success"
-   # listWriter = csv.DictWriter(open('/Users/broberts/Desktop/Sum_CSP1_output.csv', 'wb'), fieldnames=itemDict[itemDict.keys()[0]].keys(), delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)   
+         if not mvparty in parties:
+            parties.append(mvparty)
+            listOfParties.writerow([mvparty])
+         listOfMv.writerow([mvno, mvname, mvparty, city])
 
 def mvinfo():
-   mv_specs()
-   print mv_ids
+   listOfMv = csv.reader(open('mvlist.csv'))
+   mv_info = []
+   for mv in listOfMv:
+      mv_info.append(mv)
+   return mv_info
+
+def getpartylist():
+   listOfParties = csv.reader(open('../partylist.csv'))
+   parties = []
+   for party in listOfParties:
+      parties.append(party)
+   return parties
 
 def mv_workcount(idmv):
    all_links1 = [gds.MV_ILK_KANUN, gds.MV_KANUN, gds.MV_ILK_MAO, gds.MV_MAO, gds.MV_ILK_GGO, gds.MV_GGO, gds.MV_ILK_GO, gds.MV_GO]
@@ -62,7 +77,7 @@ def mv_workcount(idmv):
    return resultset
 
 def gather_all_work():
-   mv_specs()
+   main()
    i = 0
    for key in mv_ids:
       mv_all_work[key] = mv_workcount(key)
@@ -77,5 +92,6 @@ def gather_all_work():
 
 if __name__=="__main__":
    # gather_all_work()
-   mv_specs()
-   # mvinfo()
+   # main()
+   mvinfo()
+   # getpartylist()
