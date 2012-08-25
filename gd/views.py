1a82d0392b947f65c30e2d38c73aa795f3a1fb9c
furiston <furiston@gmail.com>
2012-08-25 18:03:32 +0300

    retrieve data from csv file

--- gd/views.py
+++ gd/views.py
@@ -1,19 +1,26 @@
 from django.shortcuts import render_to_response
-from getdemocracy import mvinfo
+from getdemocracy import mvinfo, getpartylist
 import gdsettings as gds
 from models import MV, Party, MVWorkCount
 
 def update_1982(request):
-	get_mv_id = mvinfo()
+	get_mv_info = mvinfo()
+	partylist = getpartylist()
 
-	for k, v in get_mv_id.iteritems():
+	# for party in partylist:
+	# 	p = Party(name=party)
+	# 	p.save()
 
-		mvq = MV(name=v, mv_id=int(k), party=Party.objects.get(pk=1), city="Ankara")
-		mvq.save()
+	# print "parties saved" 
+
+	# for k, v in get_mv_info.iteritems():
+
+	# 	mvq = MV(name=v, mv_id=int(k), party=Party.objects.get(pk=1), city="Ankara")
+	# 	mvq.save()
 
 	return render_to_response('index.html', {'update_status':"updated"})
 
 def home(request):
-	get_mv_id = mvinfo()
+	mv_info = mvinfo()
 	ilk_kanun = gds.MV_ILK_KANUN
-	return render_to_response('index.html', {'get_mv_id':get_mv_id, 'ilk_kanun':ilk_kanun})
+	return render_to_response('index.html', {'mv_info':mv_info, 'ilk_kanun':ilk_kanun})
