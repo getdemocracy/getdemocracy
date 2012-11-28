9342a13706b81f4d41f931322467019355a9301a
furiston <furiston@gmail.com>
2012-11-28 02:56:57 +0200

    just add homejson url to see how django.core.serialzer works for json

--- getDemocracy/urls.py
+++ getDemocracy/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -7,7 +8,7 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'gd.views.home', name='home'),
-    url(r'^update_1982', 'gd.views.update_1982')
+    url(r'^homejson', 'gd.views.homejson', name='home'),
     # url(r'^getDemocracy/', include('getDemocracy.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
@@ -16,3 +17,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += staticfiles_urlpatterns()
