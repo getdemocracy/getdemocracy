e1c9130f487ef151d218daec308687d4fc087a09
furiston <furiston@gmail.com>
2012-11-28 02:55:42 +0200

    some basic rehersal to retrieve data from db and send it to a view *** just a trial

--- gd/views.py
+++ gd/views.py
@@ -1,29 +1,16 @@
-from django.shortcuts import render_to_response
-from getdemocracy import main as updateMvInfo, mvinfo
-import gdsettings as gds
-from models import MV, Party, MVWorkCount
-import datetime, glob
-
-def update_1982(request):
-	updateMvInfo()
-
-	# for party in partylist:
-	# 	p = Party(name=party)
-	# 	p.save()
-
-	# print "parties saved" 
-
-	# for k, v in get_mv_info.iteritems():
-
-	# 	mvq = MV(name=v, mv_id=int(k), party=Party.objects.get(pk=1), city="Ankara")
-	# 	mvq.save()
-
-	return render_to_response('index.html', {'update_status':"updated"})
+from django.shortcuts import render_to_response, get_object_or_404
+from django.http import HttpResponse
+from django.template import RequestContext
+from models import MV, Mv_records
+from django.db.models import Count
+from django.core import serializers
 
 def home(request):
-	allfiles = []
-	for files in glob.glob('*.csv'):
-		allfiles.append(files)
-	mv_info = mvinfo(str(allfiles[0]))
-	ilk_kanun = gds.MV_ILK_KANUN
-	return render_to_response('index.html', {'mv_info':mv_info, 'filelist':allfiles})
+	mv_info = MV.objects.values('party').annotate(dcount=Count('party'))
+	city_info = MV.objects.values('city').annotate(dcount=Count('city'))
+	return render_to_response('index.html', {'mv_info': mv_info, 'city_info': city_info}, context_instance=RequestContext(request))
+
+def homejson(request):
+	mv_i = MV.objects.values('party').annotate(dcount=Count('party'))
+	mvx = serializers.serialize('json', mv_i)
+	return HttpResponse(mvx, mimetype='application/json')
